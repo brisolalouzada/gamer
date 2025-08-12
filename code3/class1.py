9229dd1bfee4bf53ac01496d89c88449df144a65
marisa060979 <taisaramos@hotmail.com>
2025-08-12 13:25:09 -0300

    adding classes generated fron StarUML

---
+++ code3/class1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from name import name
+
+
+class Class1(name):
+    def __init__(self):
+        pass
+
+    def move(self, ):
+        pass
---
+++ code3/enemy.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from name import name
+
+
+class Enemy(name):
+    def __init__(self):
+        pass
+
+    def move(self, ):
+        pass
