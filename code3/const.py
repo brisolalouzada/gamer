8b8a335efed5d7a67709de036652d177c5aad94e
marisa060979 <taisaramos@hotmail.com>
2025-08-16 13:49:54 -0300

    creating asset folder inserting all assets creating the menu: background, sound and fonts creating the const file

---
+++ code3/const.py
@@ -0,0 +1,14 @@
+COLOR_GREEN = (50, 255, 0)
+COLOR_WHITE = (255, 255, 255)
+
+MENU_OPTION = ("NEW GAME 1P",
+               "NEW GAME 2P - COOPERATIVE",
+               "NEW GAME 2P - COOPERATIVE",
+               "SCORE",
+               "EXIT")
+
+
+
+
+WIN_WIDTH = 485
+WIN_HEIGHT = 300
--- code3/game.py
+++ code3/game.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from typing import Any
+
 import pygame
 
+from code3.const import WIN_WIDTH, WIN_HEIGHT
 from code3.menu import Menu
 
 
 class Game:
     def __init__(self):
-
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
-    def run(self, ):
+    def run(self):
+
 
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
-              for event in pygame.event.get():
-                if event.type == pygame.QUIT:
 
-                 pygame.quit()
-                 quit()
 
